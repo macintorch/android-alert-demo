1013c324dbcff9e9132dcbf953f36e912e83bd2a
Ainor Syahrizal <macintorch@gmail.com>
2017-01-04 15:08:32 +0800

    create alert dialog

--- app/src/main/java/ainor/com/my/alertdemo/MainActivity.java
+++ app/src/main/java/ainor/com/my/alertdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package ainor.com.my.alertdemo;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Are you sure?")
+                .setMessage("Do you confirm?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Its done", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("No",null)
+                .show();
     }
 }
